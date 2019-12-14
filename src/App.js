a8eee4820bf42f3242a65fee1025789b58830aa5
ksharifbd <ksharifbd@gmail.com>
2019-12-14 20:43:35 +0600

    applies container padding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import styled from '@material-ui/core/styles/styled';
 import ExchangeRate from './components/ExchangeRate';
 
 function App() {
+  const Container = styled('div')({
+    padding: '1.5rem',
+  });
+
   return (
-    <div>
-      <CssBaseline>
+    <CssBaseline>
+      <Container>
         <ExchangeRate />
-      </CssBaseline>
-    </div>
+      </Container>
+    </CssBaseline>
   );
 }
 
