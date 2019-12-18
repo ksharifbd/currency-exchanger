1d05c730dde517678481af5b6ccd6d592603dc0b
ksharifbd <ksharifbd@gmail.com>
2019-12-18 13:28:09 +0600

    moves api poll interval to the env

--- src/sagas/exchangeRate.js
+++ src/sagas/exchangeRate.js
@@ -31,7 +31,7 @@ function* getExchangeRatesSaga() {
         payload: response.data,
       });
 
-      yield delay(30000000);
+      yield delay(process.env.REACT_APP_API_POLL_INTERVAL || 10000);
     } catch (error) {
       yield put({
         type: actions.FETCH_EXCHANGE_RATES_ERROR,
